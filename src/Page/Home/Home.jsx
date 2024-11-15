691ad401e1c99c77ca7104803cf470a088e7b08c
Zamirbekkg <perizat9595kg78@gmail.com>
2024-11-15 06:01:15 +0100

    Add files via upload

---
+++ src/Page/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../Home/Home.css'
+
+const Home = () => {
+    return (
+        <div className='Conreiner'>
+            <div className='nomber'>
+                <p className='nomberP'>8 (812) 123-45-67    |    Работаем 7 дней в неделю    |    9:00 — 18:00  </p>
+            </div>
+            <div>
+                <p>Войти / Регистрация</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
